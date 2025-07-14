cadc7325d18c746f4165611d1ce4c31477261ec3
mohammad aldaker <mohammaddakerlovebasem@gmail.com>
2025-07-14 13:06:20 +0300

    create

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import NavigationMenuDemo from "@/components/base/nav-bar";
+import { Card, CardContent } from "@/components/ui/card";
+
+export default function page() {
+  return (
+    <Card >
+      <CardContent>
+        
+      </CardContent>
+    </Card>
+  );
+}
---
+++ src/data.ts
@@ -0,0 +1,21 @@
+export const resturant = {
+  default_data: {
+    name: "",
+    logo: "",
+    description: "",
+    open_at: "",
+    close_at: "",
+    slug: "",
+  },
+
+  products:[
+    {
+        prod_id : "" , 
+        prod_name : "" ,
+        prod_prepairing_time : "" ,
+        prod_img : "" ,
+        prod_description : ""
+    }
+  ]
+  ,
+};
